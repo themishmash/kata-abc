154a1a5ab88bbd14659170a4d39f2590d094eaa8
Michelle <wong.mich.c@gmail.com>
2020-03-26 19:52:16 +1100

    test

--- kata-abc/kata-abc/Solution.cs
+++ kata-abc/kata-abc/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace kata_abc
 {
@@ -7,11 +8,13 @@ namespace kata_abc
     {
         public static void Solution()
         {
-            var blockList = new List<Tuple<string, string>>
+
+            //var blockList = new List<Tuple<string, string>>
                 //Tuple<string, string>[] blockList =
-                {
-                    Tuple.Create("B", "O"),
-                    Tuple.Create("X", "K"),
+                //{
+                    //Tuple.Create("B","O"),
+                   // Tuple.Create("X","K"),
+/*
                     Tuple.Create("D", "Q"),
                     Tuple.Create("C", "P"),
                     Tuple.Create("N", "A"),
@@ -30,25 +33,58 @@ namespace kata_abc
                     Tuple.Create("L", "Y"),
                     Tuple.Create("P", "C"),
                     Tuple.Create("Z", "M"),
-                };
+*/
+               // };
             
-            Console.WriteLine(blockList.Count);
-            
+           // Console.WriteLine(blockList.Count);
+
+			List<string> blocks = new List<string>();
+					blocks.Add("BO");
+                   	blocks.Add("XK");
+                   	blocks.Add("DQ");
+                    blocks.Add("CP");
+                    blocks.Add("NA");
+                    blocks.Add("GT");
+                    blocks.Add("RE");
+                    blocks.Add("TG");
+                    blocks.Add("QD");
+                    blocks.Add("FS");
+                    blocks.Add("JW");
+                    blocks.Add("HU");
+                    blocks.Add("VI");
+                    blocks.Add("AN");
+                    blocks.Add("OB");
+                    blocks.Add("ER");
+                    blocks.Add("FS");
+                    blocks.Add("LY");
+                    blocks.Add("PC");
+                    blocks.Add("ZM");
+
+			       
             
             Console.WriteLine("What word are you trying to make?");
             var userInput = Console.ReadLine().ToUpper();
 
-            //split word into letters
-            char userLetter;
-            foreach (char c in userInput)
+            foreach (string block in blocks)
             {
-                userLetter = c;
-
-                Console.WriteLine(userLetter);
+                if (block.Contains(userInput))
+                {
+                  
+                    Console.WriteLine(block);
+                }
                 
             }
             
-            
+
+
+
+
+
         }
+            //Console.WriteLine(blockList);
+
+            
+                
+        
     }
 }
